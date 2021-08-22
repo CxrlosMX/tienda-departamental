b58c673ec06e1ecb18cdb9f846ccfc7f12f17f64
CxrlosMX <LuisCRendon131@gmail.com>
2021-08-21 19:55:09 -0500

    Inicio de la aplicacion

---
+++ src/areadepartamental/Producto.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package areadepartamental;
+
+/**
+ *
+ * @author: CxrlosMX
+ * @Git-Hub: https://github.com/CxrlosMX
+ * @Phone: 953-212-97-27
+ * @Email: LuisCRendon131@gmail.com
+ * @Date: 21/08/2021
+ * 
+ */
+public class Producto {
+
+}
